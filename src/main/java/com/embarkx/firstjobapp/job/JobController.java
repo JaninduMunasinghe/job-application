cdbbb6972571b69f28aec9ba091a4bd28df95a2c
JaninduMunasinghe <munasin-ps20057@stu.kln.ac.lk>
2025-06-25 18:00:10 +0530

    create two endpoints

--- src/main/java/com/embarkx/firstjobapp/job/JobController.java
+++ src/main/java/com/embarkx/firstjobapp/job/JobController.java
@@ -1,5 +1,25 @@
 package com.embarkx.firstjobapp.job;
 
-public class JobController {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobController {
+    private List<Job> jobs = new ArrayList<>();
+
+    @GetMapping("/jobs")
+    public List<Job> findAll(){
+        return jobs;
+    }
+
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job){
+        jobs.add(job);
+        return "Job added successfully";
+    }
 }
